2da829b1e0c2ca6ec0163f3e1d8e5b0b33ce8451
Porter Wilcox <wilcox.porter@gmail.com>
2023-07-30 22:54:05 -0600

    ContentTypes and Generic Relationships and Keys

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Post, Tag
+from .models import Post, Tag, Comment
 
 
 admin.site.register(Tag)
+
+
+admin.site.register(Comment)
 
 
 class PostAdmin(admin.ModelAdmin):
