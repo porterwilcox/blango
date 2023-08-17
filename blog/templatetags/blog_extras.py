5ed55a3e5af2901fd49188729fe80337aa455faa
Porter Wilcox <wilcox.porter@gmail.com>
2023-08-16 21:30:56 -0600

    Finish custom filters

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,15 +4,19 @@ from django.contrib.auth import get_user_model
 user_model = get_user_model()
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
 
 
 @register.filter
-def author_details(author):
+def author_details(author, current_user=None):
 
     if not isinstance(author, user_model):
         return ""
     
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
     if author.first_name and author.last_name:
         name = f"{author.first_name} {author.last_name}"
     else:
